80d7650f1be3cf6b926e924cc3ca96f4c78a5df0
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-26 00:35:07 +0430

    importing files

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+const api = axios.create({
+  baseURL: 'https://arha83.pythonanywhere.com/api/vars/'
+})
+
+
+class App extends Component {
+
+  
+  state = {
+    courses:[]
+  }
+
+
+  constructor(){
+    super();
+    api.get('/').then(res =>{
+      this.setState({courses: res.data})
+    })
+  }
+
+
+  render (){return (
+    <div className="App">
+      <main className="App-header">
+        <table>
+          <tr>
+          <th>id</th>
+          <th>text</th>
+          <th>number</th>
+          <th>bit</th>
+          <th>date</th>
+          </tr>
+          {this.state.courses.map((course) => (
+            <tr>
+           <td>{course.id}</td>
+           <td>{course.text}</td>
+            <td>{course.num}</td>
+           <td>{course.bit}</td>
+            <td>{course.date}</td>
+            </tr>
+          ))}
+        </table>
+        
+      </main>
+    </div>
+  );}
+}
+
+export default App;
